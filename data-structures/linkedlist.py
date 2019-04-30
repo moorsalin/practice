efc2a79e7d3a5d648d19d90571c0d778ac4237c6
moorsalin <moorsalin.munshi@gmail.com>
2019-04-29 22:39:11 -0400

    linked list implementation start

---
+++ data-structures/linkedlist.py
@@ -0,0 +1,12 @@
+class Node:
+    def __init__(self,data=None):
+        self.data = data
+        self.next = None
+
+class SinglyLinkedList:
+    def __init__(self):
+        self.head = Node()
+
+    def add(data):
+        p = Node(data)
+        
