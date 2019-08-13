66a19d2aea1c6cf5f65d7445236f006b99e97c38
moorsalin <moorsalin.munshi@gmail.com>
2019-08-13 14:17:42 -0400

    practice linked list implementation

--- programming/data-structures/linkedlist.c
+++ programming/data-structures/linkedlist.c
@@ -1,40 +1,29 @@
 #include <stdio.h>
-#include <malloc.h>
+#include <stdlib.h>
 
 struct node {
     int data;
     struct node* next;
 };
 
-struct node* BuildOneTwoThree();
-int Length(struct node* head);
-void Push(struct node** headRef, int newData);
+struct node* BuildOneTwoThree() {
+    struct node* h = malloc(sizeof(struct node));
+    struct node* s = malloc(sizeof(struct node));
+    struct node* t = malloc(sizeof(struct node));
+    
+    h->data = 1;
+    h->next = s;
+    
+    s->data = 2;
+    s->next = t;
+    
+    t->data = 3;
+    t->next = NULL;
+    
+    return h;
+}
 
 int main() {
+    printf("Yes I can write hello world in C\n");
     return 0;
-}
-
-int Length(struct node* head) {
-    int count = 0;
-    struct node* cur;
-
-    for (cur = head; cur != NULL; cur = cur->next) {
-        count++;
-    }
-    return count;
-}
-
-void Push(struct node** headRef, int newData) {
-    struct node* newNode = (struct node*) malloc(sizeof(struct node));
-    newNode->data = newData;
-    newNode->next = (*headRef);
-    (*headRef) = newNode;
-}
-
-struct node* BuildOneTwoThree() {
-    struct node* head = NULL;
-    for (int i = 3; i > 0; i--) {
-        Push(&head,i);
-    }
-    return head;
 }
